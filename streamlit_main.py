e3c5a996cca6a6e64f2d6a40c6a8163a96ea6a5a
Ehsan-sas <ehsan.asadollahiasl@gmail.com>
2024-10-04 12:07:48 -0400

    Update streamlit_main.py

--- streamlit_main.py
+++ streamlit_main.py
@@ -87,7 +87,7 @@ st.write('- IEEE format')
 st.header('Gantt Chart')
 if st.button('Access the File'):
     # Media
-    st.image('./GANTT.png', width=1200)
+    st.image('./GANTT.png', width=1600)
     
 
 
